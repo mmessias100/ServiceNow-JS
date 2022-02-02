23d51c90d051251e02cf2d7f643fbe04230f884c
Morris Messias <messiasmorris@gmail.com>
2022-02-02 15:46:16 +0000

    added DOM exercises and challenge

---
+++ wednesday_startpoint/calculatorChallenge/calculator.js
@@ -0,0 +1,42 @@
+`use strict`;
+import * as DOM from './domchallenge.js';
+
+DOM.addButton.onclick = () => add(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.subButton.onclick = () => sub(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.divButton.onclick = () => div(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.multButton.onclick = () => mult(DOM.inputOne.value, DOM.inputTwo.value);
+
+function showAnswer(int){
+
+    let addResult = document.createElement(`li`);
+    addResult.innerHTML = int;
+    DOM.outPut.appendChild(addResult)
+}
+
+function add (num1, num2){
+
+    let result = 0;
+    result = +num1 + +num2;
+    showAnswer(result);
+}
+
+function sub (num1, num2){
+
+    let result = 0;
+    result = +num1 - +num2;
+    showAnswer(result);
+}
+
+function div (num1, num2){
+
+    let result = 0;
+    result = +num1 / +num2;
+    showAnswer(result);
+}
+
+function mult (num1, num2){
+
+    let result = 0;
+    result = +num1 * +num2;
+    showAnswer(result);
+}
---
+++ wednesday_startpoint/calculatorChallenge/domchallenge.js
@@ -0,0 +1,7 @@
+export const inputOne = document.getElementById(`input_1`);
+export const inputTwo = document.getElementById(`input_2`);
+export const outPut = document.getElementById(`list_output`);
+export const addButton = document.getElementById(`button_add`);
+export const subButton = document.getElementById(`button_subtract`);
+export const multButton = document.getElementById(`button_multiply`);
+export const divButton = document.getElementById(`button_divide`);
